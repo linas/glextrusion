b1d65e6020172c69d22442c6be4077339ab41674
Linas Vepstas <linasvepstas@gmail.com>
2023-08-18 17:43:10 -0500

    Fix aggressive-optimization compiler warning

--- examples/twistoid.c
+++ examples/twistoid.c
@@ -95,8 +95,8 @@ static void init_toid1_line (void)
 
 #define NUM_TWIS_PTS (20)
 
-double twistation [NUM_TWIS_PTS][2];
-double twist_normal [NUM_TWIS_PTS][2];
+double twistation [NUM_TWIS_PTS+1][2];
+double twist_normal [NUM_TWIS_PTS+1][2];
 
 static void init_tripples (void)
 {
